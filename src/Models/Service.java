34bde0725a84522a2669e942320816fd79d61052
tuyen5tuoi1412 <75303161+tuyen5tuoi1412@users.noreply.github.com>
2021-10-29 17:58:57 +0700

    Fix service

--- src/Models/Service.java
+++ src/Models/Service.java
@@ -2,6 +2,7 @@ package Models;
 
 public class Service {
     private String id_Service;
+    private String name_Service;
     private String Staff_Name_Perform;
     private double Price_Service;
 
@@ -9,20 +10,26 @@ public class Service {
     public String toString() {
         return "Service{" +
                 "id_Service='" + id_Service + '\'' +
+                ", name_Service='" + name_Service + '\'' +
                 ", Staff_Name_Perform='" + Staff_Name_Perform + '\'' +
                 ", Price_Service=" + Price_Service +
                 '}';
     }
 
-    public Service() {
-    }
-
-    public Service(String id_Service, String staff_Name_Perform, double price_Service) {
+    public Service(String id_Service, String staff_Name_Perform, double price_Service, String name_Service) {
         this.id_Service = id_Service;
         Staff_Name_Perform = staff_Name_Perform;
         Price_Service = price_Service;
+        this.name_Service = name_Service;
     }
 
+    public String getName_Service() {
+        return name_Service;
+    }
+
+    public void setName_Service(String name_Service) {
+        this.name_Service = name_Service;
+    }
     public String getId_Service() {
         return id_Service;
     }
